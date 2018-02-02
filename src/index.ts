8c02bb0601a03e2fe1f6e2ab91a4b4e7ae1c9973
Unknown <domenikirrgang@web.de>
2018-02-02 18:49:33 +0100

    trying out env variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-console.log("Hello World, I am LoChat. Im am running in " + process.env.NODE_ENV + " mode.");
+
+console.log("Hello World, I am LoChat. Im am running in " + process.env.NODE_ENV + " mode. ABCENV = " + process.env.ABCENV);
